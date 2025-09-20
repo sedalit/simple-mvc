bf85386c6581e59f9d3a763575eb161b7cd23949
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-09-20 16:24:00 +1000

    Update render function

--- core/View.php
+++ core/View.php
@@ -11,12 +11,14 @@ class View {
         $this->layout = $layout;
     }
 
-    public function render(string $viewName, array $data = [], string $layout = '')
+    public function render(string $viewName, array $data = [], string $layout = '') : bool|string
     {
         $viewFile = VIEWS . "/{$viewName}.php";
         if (is_file($viewFile)) {
             extract($data);
+            ob_start();
             require_once $viewFile;
+            return ob_get_clean();
         } else {
             throw new \Exception("View file not found: {$viewFile}");
         }
