da4da293347ae7102d60abce558e7dfd2d0b9f8e
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-09-20 14:31:21 +1000

    Add getMethod to Request class

--- core/Request.php
+++ core/Request.php
@@ -3,10 +3,27 @@
 namespace PHPFramework;
 
 class Request {
+    protected const METHOD_HIDDEN_KEY = '_method';
     protected string $uri;
 
     public function __construct(string $uri)
     {
         $this->uri = trim(urldecode($uri), '/');
+    }
+
+    public function getPath() : string
+    {
+        return $this->uri;
+    }
+
+    public function getMethod() : string
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if (isset($_POST[self::METHOD_HIDDEN_KEY])) {
+            $method = $_POST[self::METHOD_HIDDEN_KEY];
+        }
+
+        return strtoupper($method);
     }
 }
