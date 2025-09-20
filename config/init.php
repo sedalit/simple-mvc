f513a84f376f697c84b89dd6025b07e9957e808f
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-09-20 12:34:00 +1000

    Create init.php and define constants

---
+++ config/init.php
@@ -0,0 +1,9 @@
+<?php
+
+define("ROOT", dirname(__DIR__));
+
+const WWW = ROOT . '/public';
+const APP = ROOT . '/app';
+const CORE = ROOT . '/core';
+const HELPERS = ROOT . '/helpers';
+const CONFIG = ROOT . '/config';
--- public/index.php
+++ public/index.php
@@ -4,4 +4,6 @@ if (PHP_MAJOR_VERSION < 8) {
     die("Require PHP version >= 8");
 }
 
+require_once __DIR__ . '/../config/init.php';
+
 echo 'Hello';
