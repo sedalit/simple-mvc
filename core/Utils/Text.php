9f14ef98138631078b79df78f37a13b651c7614e
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-09-27 18:46:21 +1000

    Create Utils namespace with slugify function

---
+++ core/Utils/Text.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PHPFramework\Utils;
+
+class Text {
+    public static function slugify(string $text) : string
+    {
+        $text = mb_strtolower($text, 'UTF-8');
+
+        if (preg_match('/[а-яё]/u', $text)) {
+            $cyr = [
+                'а'=>'a','б'=>'b','в'=>'v','г'=>'g','д'=>'d',
+                'е'=>'e','ё'=>'e','ж'=>'zh','з'=>'z','и'=>'i',
+                'й'=>'y','к'=>'k','л'=>'l','м'=>'m','н'=>'n',
+                'о'=>'o','п'=>'p','р'=>'r','с'=>'s','т'=>'t',
+                'у'=>'u','ф'=>'f','х'=>'h','ц'=>'ts','ч'=>'ch',
+                'ш'=>'sh','щ'=>'sch','ъ'=>'','ы'=>'y','ь'=>'',
+                'э'=>'e','ю'=>'yu','я'=>'ya'
+            ];
+            $text = strtr($text, $cyr);
+        } else {
+            $text = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $text);
+        }
+ 
+        $text = preg_replace('/[^a-z0-9- ]+/', '', $text);
+        $text = preg_replace('/[^a-z0-9-]+/', '-', $text);
+        $text = trim($text, '-');
+
+        return $text ?: '';
+    }
+}
