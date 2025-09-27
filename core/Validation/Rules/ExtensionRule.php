08b385063c45e164b531aa783640070bf5135b84
Vladislav Agarkov <vlad.agarkov@alto-ai.ru>
2025-09-27 20:18:07 +1000

    Add extension rule

---
+++ core/Validation/Rules/ExtensionRule.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace PHPFramework\Validation\Rules;
+
+use PHPFramework\Validation\ValidationRule;
+use PHPFramework\Utils\File;
+
+class ExtensionRule extends ValidationRule
+{
+    protected string $message = "File :fieldname: has wrong extension. Allowed :allowed:.";
+
+    protected array $allowed = [];
+
+    public function __construct(string $field, mixed $value, array $params = [])
+    {
+        parent::__construct($field, $value, $params);
+        $this->allowed = explode(',', $params[0]);
+        $this->allowed = array_map('strtolower', $this->allowed);
+
+        $this->message = str_replace([':fieldname:', ':allowed:'], [self::FIELDNAME_PLACEHOLDER, $params[0]], $this->message);
+    }
+
+    public static function key() : ?string
+    {
+        return 'extension';
+    }
+
+    public function passes(): bool
+    {
+        $extension = File::getExtension('logo.my.JPEG');
+        $extension = strtolower($extension);
+
+        return in_array($extension, $this->allowed);
+    }
+}
